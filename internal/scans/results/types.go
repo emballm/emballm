b41e6277f6dbe48623de9c20149e87862a18e800
gabeaboy <gabriel.aboy@harness.io>
2024-05-30 10:06:45 -0600

    Add severity and remove reference ids

--- internal/scans/results/types.go
+++ internal/scans/results/types.go
@@ -9,10 +9,9 @@ type CodeDetails struct {
 }
 
 type RawVulnerability struct {
-	Title    string `json:"title"`    // The name or title of the vulnerability found
-	RuleID   string `json:"rule_id"`  // Identifier that is a snake title of the issue
-	Severity string `json:"severity"` // A measure of the potential impact of the vulnerability, often classified as low, medium, high, or critical
-	// ReferenceIdentifiers []string    `json:"reference_identifiers"` // Use the NVD database to find the CVE that corresponds with the vulnerability
+	Title            string      `json:"title"`             // The name or title of the vulnerability found
+	RuleID           string      `json:"rule_id"`           // Identifier that is a snake title of the issue
+	Severity         string      `json:"severity"`          // A measure of the potential impact of the vulnerability, often classified as low, medium, high, or critical
 	RemediationSteps string      `json:"remediation_steps"` // Instructions or actions needed to fix or mitigate the vulnerability
 	IssueDescription string      `json:"issue_description"` // A detailed explanation of the vulnerability, including how it can be exploited and the potential consequences
 	ShortDescription string      `json:"short_description"` // A brief summary of the vulnerability
@@ -21,21 +20,15 @@ type RawVulnerability struct {
 	CodeSnippet      string      `json:"code_snippet"`      // A small segment or excerpt of the source code where the vulnerability is located
 }
 
-type ReferenceIdentifier struct {
-	Type string `json:"type"` // The type of the reference identifier (e.g., cwe)
-	ID   string `json:"id"`   // The ID of the reference identifier (e.g., 79)
-}
-
 type Issue struct {
-	Title    string `json:"title"`
-	Severity string `json:"severity"`
-	// ReferenceIdentifiers []ReferenceIdentifier `json:"reference_identifiers"`
-	FileName         string `json:"file_name"`
-	RemediationSteps string `json:"remediation_steps"`
-	IssueDescription string `json:"issue_description"`
-	ShortDescription string `json:"short_description"`
-	LineNumber       int    `json:"line_number"`
-	CodeSnippet      string `json:"code_snippet"`
+	Title            string  `json:"title"`
+	Severity         float32 `json:"severity"`
+	FileName         string  `json:"file_name"`
+	RemediationSteps string  `json:"remediation_steps"`
+	IssueDescription string  `json:"issue_description"`
+	ShortDescription string  `json:"short_description"`
+	LineNumber       int     `json:"line_number"`
+	CodeSnippet      string  `json:"code_snippet"`
 }
 
 type Meta struct {
